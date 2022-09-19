b606f0a9d77aeb4a06b84d998f8983e2bbfbf8b9
DenisWm <denismamoni@hotmail.com>
2022-09-19 09:30:20 -0400

    Restante da implementaćão

--- src/main/java/com/devsuperior/dslearnbds/entities/Enrollment.java
+++ src/main/java/com/devsuperior/dslearnbds/entities/Enrollment.java
@@ -1,88 +1,129 @@
 package com.devsuperior.dslearnbds.entities;
 
-import com.devsuperior.dslearnbds.pk.EnrollmentPK;
-
-import javax.persistence.*;
-import java.io.Serializable;
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.devsuperior.dslearnbds.entities.pk.EnrollmentPK;
 
 @Entity
 @Table(name = "tb_enrollment")
-public class Enrollment implements Serializable {
-    private static final long serialVersionUID = 1L;
+public class Enrollment {
 
+	@EmbeddedId
+	private EnrollmentPK id = new EnrollmentPK();
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant enrollMoment;
 
-    @EmbeddedId
-    private EnrollmentPK id = new EnrollmentPK();
-    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
-    private Instant enrollMoment;
-    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
-    private Instant refundMoment;
-    private boolean available;
-    private boolean onlyUpdate;
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant refundMoment;
+	private boolean available;
+	private boolean onlyUpdate;
 
-    @ManyToMany(mappedBy = "enrollmentDone")
-    private Set<Lesson> lessonsDone = new HashSet<>();
+	@ManyToMany(mappedBy = "enrollmentsDone")
+	private Set<Lesson> lessonsDone = new HashSet<>();
+	
+	@OneToMany(mappedBy = "enrollment")
+	private List<Deliver> deliveries = new ArrayList<>();
+	
+	public Enrollment() {
+	}
 
-    public Enrollment() {
-    }
+	public Enrollment(User user, Offer offer, Instant enrollMoment, Instant refundMoment, boolean available,
+			boolean onlyUpdate) {
+		super();
+		id.setUser(user);
+		id.setOffer(offer);
+		this.enrollMoment = enrollMoment;
+		this.refundMoment = refundMoment;
+		this.available = available;
+		this.onlyUpdate = onlyUpdate;
+	}
 
-    public Enrollment(User user, Offer offer, Instant enrollMoment, Instant refund, boolean available, boolean onlyUpdate) {
-        this.id.setUser(user);
-        this.id.setOffer(offer);
-        this.enrollMoment = enrollMoment;
-        this.refundMoment = refund;
-        this.available = available;
-        this.onlyUpdate = onlyUpdate;
-    }
+	public User getStudent() {
+		return id.getUser();
+	}
+	
+	public void setStudent(User user) {
+		id.setUser(user);
+	}
 
-    public User getStudent(){
-       return id.getUser();
-    }
+	public Offer getOffer() {
+		return id.getOffer();
+	}
+	
+	public void setOffer(Offer offer) {
+		id.setOffer(offer);
+	}
+	
+	public Instant getEnrollMoment() {
+		return enrollMoment;
+	}
 
-    public void setStudent(User user){
-        this.id.setUser(user);
-    }
+	public void setEnrollMoment(Instant enrollMoment) {
+		this.enrollMoment = enrollMoment;
+	}
 
-    public Offer getOffer(){
-        return id.getOffer();
-    }
+	public Instant getRefundMoment() {
+		return refundMoment;
+	}
 
-    public void setOffer(Offer offer){
-        this.id.setOffer(offer);
-    }
+	public void setRefundMoment(Instant refundMoment) {
+		this.refundMoment = refundMoment;
+	}
 
-    public Instant getEnrollMoment() {
-        return enrollMoment;
-    }
+	public boolean isAvailable() {
+		return available;
+	}
 
-    public void setEnrollMoment(Instant enrollMoment) {
-        this.enrollMoment = enrollMoment;
-    }
+	public void setAvailable(boolean available) {
+		this.available = available;
+	}
 
-    public Instant getRefundMoment() {
-        return refundMoment;
-    }
+	public boolean isOnlyUpdate() {
+		return onlyUpdate;
+	}
 
-    public void setRefundMoment(Instant refundMoment) {
-        this.refundMoment = refundMoment;
-    }
+	public void setOnlyUpdate(boolean onlyUpdate) {
+		this.onlyUpdate = onlyUpdate;
+	}
 
-    public boolean isAvailable() {
-        return available;
-    }
+	public List<Deliver> getDeliveries() {
+		return deliveries;
+	}
 
-    public void setAvailable(boolean available) {
-        this.available = available;
-    }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
 
-    public boolean isOnlyUpdate() {
-        return onlyUpdate;
-    }
-
-    public void setOnlyUpdate(boolean onlyUpdate) {
-        this.onlyUpdate = onlyUpdate;
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Enrollment other = (Enrollment) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 }
